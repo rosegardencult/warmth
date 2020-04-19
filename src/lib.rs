343295ff3250d5dc50aa7fa6f388a8e4a4a3c160
juniper wilde <juniper.r.wilde@gmail.com>
2020-04-18 17:43:37 -0700

    init!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright (c) 2020 rose garden cult All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//===----------------------------------------------------------------------===//
+//
+// Warmth Engine Library root entry point
+//
+//===----------------------------------------------------------------------===//
+
+#![doc(issue_tracker_base_url = "https://github.com/rosegardencult/fluff/issues/")]
